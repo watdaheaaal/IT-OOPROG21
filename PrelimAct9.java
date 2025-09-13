b3757c8310beef0ce6d8b31e9696f02cc9e38c28
James Gabriel Turceno <128990947+watdaheaaal@users.noreply.github.com>
2025-09-13 11:41:57 +0800

    Create PrelimAct9.java

---
+++ PrelimAct9.java
@@ -0,0 +1,15 @@
+/*
+  Name: Turceno, James Gabriel B.
+  Course/Year: BSIT - II
+  Date: Saturday, 13th day of September, Year 2025
+  
+  Description: Movie Ticket Pricing Write a program that:
+  Accepts movie type (1 for Regular, 2 for 3D, 3 for IMAX) and age.
+  Use a switch case on movie type:
+  Inside each case, use a nested condition:
+  If age < 12 or age > 60, apply 50% discount.
+  Otherwise, charge full price.
+  
+*/
+
+import 
